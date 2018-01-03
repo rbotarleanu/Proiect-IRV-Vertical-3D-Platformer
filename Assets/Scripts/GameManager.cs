a06925884c39ab507f0f2e05b75d8a8da59c2d94
Robert-Mihai BOTARLEANU <robertbotarleanu@gmail.com>
2018-01-03 20:29:35 +0200

    Add Scene management

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+/* Singleton that controls game managers and scene flow. */
+public class GameManager : MonoBehaviour {
+
+    /* Only one instance per game. */
+    private static GameManager instance;
+
+    public static GameManager GetInstance()
+    {
+        return instance;
+    }
+
+    private void Awake()
+    {
+        if (!instance)
+        {
+            Debug.Log("Instantiating new GameManager");
+            instance = this;
+            DontDestroyOnLoad(this);
+        } else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            LevelManager.LoadSceneAsync("Scene1");
+        }
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            LevelManager.LoadSceneAsync("Scene2");
+        }
+    }
+ }
---
+++ Assets/Scripts/Managers/LevelManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager {
+
+    private static AsyncOperation loadingInfo;
+    public delegate void LoadChange(float value);
+    public static event LoadChange OnLoadChange;
+
+    public delegate void LoadStart();
+    public static event LoadStart OnLoadStart;
+
+    public static IEnumerator Update()
+    {
+        while (!loadingInfo.isDone)
+        {
+            OnLoadChange(loadingInfo.progress);
+            yield return new WaitForEndOfFrame();
+        }
+
+        OnLoadChange(loadingInfo.progress);
+    }
+
+    public static void LoadSceneAsync(string sceneIndex)
+    {
+        // OnLoadStart(); 
+        loadingInfo = SceneManager.LoadSceneAsync(sceneIndex, LoadSceneMode.Single);
+        GameManager.GetInstance().StartCoroutine(Update());
+    }
+
+    public static void LoadScene(string sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex, LoadSceneMode.Single);
+    }
+}
