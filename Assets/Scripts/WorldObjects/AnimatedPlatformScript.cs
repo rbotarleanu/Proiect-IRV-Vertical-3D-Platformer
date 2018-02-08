0a53313570e6e3d0388fbb22449ed59d27f6123a
Robert Botarleanu <robertbotarleanu@gmail.com>
2018-02-08 17:36:33 +0200

    Make UI panells transparent

---
+++ Assets/Scripts/WorldObjects/AnimatedPlatformScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatedPlatformScript : MonoBehaviour {
+
+    public GameObject Platform;
+    private Animation PlatformAnimation;
+
+	// Use this for initialization
+	void Start () {
+        PlatformAnimation = Platform.GetComponent<Animation>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Start");
+        PlatformAnimation.Play();
+    }
+}
